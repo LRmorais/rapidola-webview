cafc06dc9463056f9590503081cc39b88a35c780
Lucas Morais <lucasmorais@MacBook-Pro-de-Lucas.local>
2021-05-06 15:47:14 -0300

    update

--- App.js
+++ App.js
@@ -1,56 +1,52 @@
-import React, { useEffect, useState } from 'react';
+import React, {useState, useRef } from 'react';
 import {
   StyleSheet,
   View,
-  KeyboardAvoidingView,
-
+  Text,
 } from 'react-native';
 
 import WebView from 'react-native-webview';
 
 
 const App = () => {
-  const [url, setUrl] = useState('https://www.rapidola.com.br')
-  const [urlA, setUrlA] = useState('https://www.rapidola.com.br')
-  const [isReloadWebView, setReloadWebView] = useState(false);
-  console.log(url)
+  const [url, setUrl] = useState('https://www.rapidola.com.br/')
+  const [urlA, setUrlA] = useState('https://www.rapidola.com.br/')
+  const [loading, setLoading] = useState(false)
+  const webViewRef = useRef(null)
 
-  useEffect(() => {
-    if (url !== urlA) {
-      setReloadWebView(!isReloadWebView);
-      setUrlA(url)
-    }
-  }, [url]);
-  
+  // if(urlA != url){
+
+  // }
+
+  // if(loading){
+  //   setTimeout(function(){ setLoading(false) }, 2000);
+  //   return <Text>Carregando</Text>
+  // }
 
   return (
 
-    <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
-      style={styles.keyboardAvoidingView}
-      keyboardVerticalOffset={0}
-
-    >
       <View style={{ flex: 1, marginBottom: 25 }}>
         <WebView
-          ref={CandleWVref => (CandleWebViewRef = CandleWVref)}
-          key={isReloadWebView}
+          ref={webViewRef}
           source={{ uri: url }}
-          onNavigationStateChange={(navState) => {
-            setUrl(navState.url)
-          }}
+          // onNavigationStateChange={(navState) => {
+          //   setUrlA(navState.url)
+          //   if(url != urlA){
+          //     setLoading(true)
+          //   }
+            
+          // }}
+          cacheEnabled={true}
+          limitsNavigationsToAppBoundDomains={false}
           geolocationEnabled={true}
           pullToRefreshEnabled={true}
           sharedCookiesEnabled={true}
-          originWhitelist={['*']}
-          useWebKit={true}
-          style={{ flex: 1, marginTop: '10%' }}
+          allowsBackForwardNavigationGestures={true}
+          useWebKit={false}
+          style={styles.content}
         />
 
-
-
       </View>
-    </KeyboardAvoidingView>
   );
 }
 
@@ -63,32 +59,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     marginBottom: 50
   },
-  buttonContainer: {
+  content: {
     flex: 1,
+    marginTop: '10%',
   },
   keyboardAvoidingView: {
     flexGrow: 1, flexShrink: 1
   },
-  addButton: {
 
-    borderColor: '#ff5722',
-    borderWidth: 1,
-    height: 70,
-    width: 60,
-    borderRadius: 10,
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    position: 'absolute',
-    bottom: 0,
-    right: 20,
-    shadowColor: "#000000",
-    shadowOpacity: 0.8,
-    shadowRadius: 2,
-    shadowOffset: {
-      height: 1,
-      width: 0
-    }
-  }
 });
 
 export default App;
