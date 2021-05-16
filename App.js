723bc013d5ee8a01d9094671a5bf8b1fb9c6e0b0
Lucas Morais <lucas.morais23ti@gmail.com>
2021-05-15 22:24:45 -0300

    update

--- App.js
+++ App.js
@@ -4,11 +4,70 @@ import {
   View,
 
 } from 'react-native';
-
 import WebView from 'react-native-webview';
+import {request,check, PERMISSIONS, RESULTS} from 'react-native-permissions';
+
 
 
 const App = () => {
+  request(PERMISSIONS.IOS.APP_TRACKING_TRANSPARENCY).then((result) => {
+    console.log(result)
+  });
+
+  request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE).then((result) => {
+    console.log(result)
+  });
+  
+  check(PERMISSIONS.IOS.APP_TRACKING_TRANSPARENCY)
+  .then((result) => {
+    switch (result) {
+      case RESULTS.UNAVAILABLE:
+        console.log('This feature is not available (on this device / in this context)');
+        break;
+      case RESULTS.DENIED:
+        console.log('The permission has not been requested / is denied but requestable');
+        break;
+      case RESULTS.LIMITED:
+        console.log('The permission is limited: some actions are possible');
+        break;
+      case RESULTS.GRANTED:
+        console.log('The permission is granted');
+        break;
+      case RESULTS.BLOCKED:
+        console.log('The permission is denied and not requestable anymore');
+        break;
+    }
+  })
+  .catch((error) => {
+    // …
+  });
+
+  check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE)
+  .then((result) => {
+    switch (result) {
+      case RESULTS.UNAVAILABLE:
+        console.log('This feature is not available (on this device / in this context)');
+        break;
+      case RESULTS.DENIED:
+        console.log('The permission has not been requested / is denied but requestable');
+        break;
+      case RESULTS.LIMITED:
+        console.log('The permission is limited: some actions are possible');
+        break;
+      case RESULTS.GRANTED:
+        console.log('The permission is granted');
+        break;
+      case RESULTS.BLOCKED:
+        console.log('The permission is denied and not requestable anymore');
+        break;
+    }
+  })
+  .catch((error) => {
+    // …
+  });
+  
+
+
   const [url, setUrl] = useState('https://www.rapidola.com.br/')
   const webViewRef = useRef();
 
