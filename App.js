b1549baef30f8fdb3d99367a67b69fbe44bfe165
Lucas Morais <lucas.morais23ti@gmail.com>
2021-05-19 10:22:12 -0300

    update

--- App.js
+++ App.js
@@ -11,30 +11,30 @@ import {request,check, PERMISSIONS, RESULTS} from 'react-native-permissions';
 
 const App = () => {
   request(PERMISSIONS.IOS.APP_TRACKING_TRANSPARENCY).then((result) => {
-    console.log(result)
+    // console.log(result)
   });
 
   request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE).then((result) => {
-    console.log(result)
+    // console.log(result)
   });
   
   check(PERMISSIONS.IOS.APP_TRACKING_TRANSPARENCY)
   .then((result) => {
     switch (result) {
       case RESULTS.UNAVAILABLE:
-        console.log('This feature is not available (on this device / in this context)');
+        // console.log('This feature is not available (on this device / in this context)');
         break;
       case RESULTS.DENIED:
-        console.log('The permission has not been requested / is denied but requestable');
+        // console.log('The permission has not been requested / is denied but requestable');
         break;
       case RESULTS.LIMITED:
-        console.log('The permission is limited: some actions are possible');
+        // console.log('The permission is limited: some actions are possible');
         break;
       case RESULTS.GRANTED:
-        console.log('The permission is granted');
+        // console.log('The permission is granted');
         break;
       case RESULTS.BLOCKED:
-        console.log('The permission is denied and not requestable anymore');
+        // console.log('The permission is denied and not requestable anymore');
         break;
     }
   })
@@ -46,19 +46,19 @@ const App = () => {
   .then((result) => {
     switch (result) {
       case RESULTS.UNAVAILABLE:
-        console.log('This feature is not available (on this device / in this context)');
+        // console.log('This feature is not available (on this device / in this context)');
         break;
       case RESULTS.DENIED:
-        console.log('The permission has not been requested / is denied but requestable');
+        // console.log('The permission has not been requested / is denied but requestable');
         break;
       case RESULTS.LIMITED:
-        console.log('The permission is limited: some actions are possible');
+        // console.log('The permission is limited: some actions are possible');
         break;
       case RESULTS.GRANTED:
-        console.log('The permission is granted');
+        // console.log('The permission is granted');
         break;
       case RESULTS.BLOCKED:
-        console.log('The permission is denied and not requestable anymore');
+        // console.log('The permission is denied and not requestable anymore');
         break;
     }
   })
@@ -67,17 +67,28 @@ const App = () => {
   });
   
 
-
-  const [url, setUrl] = useState('https://www.rapidola.com.br/')
+  
+  const [url, setUrl] = useState("https://www.rapidola.com.br/")
   const webViewRef = useRef();
 
+  const INJECTED_JAVASCRIPT = `<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyDPbhTDRQ2WCCMkkL9S3KhOBozOVMhBMYQ&callback=loaderCB01621369257228&libraries=places&v=3&language=en"></script>`;
+
+  var string = url.toString();
+  var result = string.includes('/checkout');
+  var time = 100
+  
+
   useEffect(() => {
-    setTimeout(function(){ webViewRef.current.reload() }, 50);
+    if(result === true){
+      time = 5000000;
+    }
+    setTimeout(function(){webViewRef.current.reload() }, time);
   }, [url]);
+
 
   return (
 
-      <View style={{ flex: 1, marginBottom: 20, marginTop: 20 }}>
+      <View style={{ flex: 1, marginBottom: 32, marginTop: 32 }}>
         <WebView
           ref={(ref) => webViewRef.current = ref} 
           source={{ uri: url }}
@@ -90,6 +101,10 @@ const App = () => {
           pullToRefreshEnabled={true}
           sharedCookiesEnabled={true}
           allowsBackForwardNavigationGestures={true}
+          javaScriptEnabled={true}
+          allowUniversalAccessFromFileURLs={true}
+          javaScriptCanOpenWindowsAutomatically={true}
+          injectedJavaScript={INJECTED_JAVASCRIPT}
           useWebKit={false}
           style={styles.content}
         />
